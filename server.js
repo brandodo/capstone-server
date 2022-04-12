bd005d7183d009198c02cbedc0963abb5f1fd502
brandodo <brandonong93@gmail.com>
2022-04-12 00:47:57 -0400

    initialized server

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+
+const expressSession = require("express-session");
+
+const cors = require("cors");
+
+const helmet = require("helmet");
+
+const passport = require("passport");
+const SpotifyStrategy = require("passport-spotify").Strategy;
+
+const knex = require("knex")(require("./knexfile.js").development);
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+require("dovenv").config();
+
+app.use(express.json());
+app.use(helmet());
+
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
+
+app.use(
+  expressSession({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
