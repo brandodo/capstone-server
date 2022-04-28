3191fed2f38a45778b1bc675b4da23b4fa8abffa
brandodo <brandonong93@gmail.com>
2022-04-28 01:43:36 -0400

    added logout route

--- routes/auth.js
+++ routes/auth.js
@@ -76,4 +76,10 @@ router.get("/refresh", (req, res) => {
   }
 });
 
+router.get("/logout", (req, res) => {
+  req.logout();
+
+  res.redirect(process.env.CLIENT_URL);
+});
+
 module.exports = router;
